43f03c09a9689dc670612dfd91667a4ed94278a8
Xu Weijiang <weijiangxu@microsoft.com>
2025-05-15 16:53:53 +0800

    add buffered async judge client

--- judge_client.py
+++ judge_client.py
@@ -1,8 +1,14 @@
 import requests
+import aiohttp
 import math
+import asyncio
 from typing import Literal
 from dataclasses import dataclass, asdict
 from concurrent.futures import ProcessPoolExecutor
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 def chunkify(iterable, size):
@@ -70,12 +76,32 @@ def _judge_batch(url: str, submissions: list[Submission], timeout: int = 3600) -
     return result.results
 
 
+async def _judge_batch_async(
+    submissions: list[Submission],
+    http: aiohttp.ClientSession,
+) -> list[SubmissionResult]:
+    if not submissions:
+        return []
+
+    batch_submission = BatchSubmission(submissions=submissions, type='batch')
+    async with http.post(
+        f'/run/long-batch',
+        json=asdict(batch_submission),
+    ) as response:
+        response.raise_for_status()
+        result = BatchSubmissionResult.from_response(await response.json())
+        return result.results
+
+
 class JudgeClient:
+    """
+    A client for the judge server.
+    This client is used to send submissions to the judge server and receive results.
+    """
     def __init__(self, url, *, max_batch_size=1000, max_workers=4):
         self.url = url
         self.max_batch_size = max_batch_size
         self.max_workers = max_workers
-        self.executor = ProcessPoolExecutor(max_workers=self.max_workers)
 
     def get_status(self, timeout: int = 10) -> ServerStatus:
         response = requests.get(
@@ -105,7 +131,7 @@ class JudgeClient:
             num_batches = max(math.ceil(len(submissions) / self.max_batch_size), self.max_workers)
             batch_size = math.ceil(len(submissions) / num_batches)
 
-            print(f'Judging {len(submissions)} submissions in {num_batches} batches of {batch_size}.')
+            logger.debug(f'Judging {len(submissions)} submissions in {num_batches} batches of {batch_size}.')
 
             pending_chunks = list(
                 chunkify(
@@ -126,9 +152,111 @@ class JudgeClient:
                         queue_timeouts.append((sub_id, sub))
                     else:
                         results[sub_id] = sub_result
-                print(f'Processed {len(results)} submissions, Got {len(queue_timeouts)} timeouts in total.')
+                logger.debug(f'Processed {len(results)} submissions, Got {len(queue_timeouts)} timeouts in total.')
 
             submissions = [sub for _, sub in queue_timeouts]
             sub_ids = [sub_id for sub_id, _ in queue_timeouts]
 
         return [results[i] for i in range(n_sumissions)]
+
+
+class BufferedJudgeClient:
+    """
+    A async client for the judge server that buffers submissions and sends them in batches.
+    """
+    def __init__(self, url, *, max_batch_size=1000, max_workers=4, timeout: int = 3600):
+        self.url = url
+        self.max_batch_size = max_batch_size
+        self.max_workers = max_workers
+        self._running = True
+        self._submission_queue = asyncio.Queue()
+        self._workers = [asyncio.create_task(self._sender_worker()) for _ in range(self.max_workers)]
+        self._http = aiohttp.ClientSession(base_url=url, timeout=aiohttp.ClientTimeout(timeout))
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self._submission_queue.join()
+        self._running = False
+        await asyncio.wait(self._workers)
+        await self._http.close()
+        # Return False to propagate exceptions
+        # in the context manager
+        return False
+
+    async def get_status(self, timeout: int = 10) -> ServerStatus:
+        async with self._http.get(
+            f'/status',
+            timeout=aiohttp.ClientTimeout(timeout),
+        ) as response:
+            response.raise_for_status()
+            return ServerStatus(**(await response.json()))
+
+    async def judge(self, submissions: list[Submission]) -> list[SubmissionResult]:
+        """
+        Send a list of submissions to the judge server and return the results.
+        """
+        if not submissions:
+            return []
+        futures = []
+        for sub in submissions:
+            future = asyncio.Future()
+            futures.append(future)
+            await self._submission_queue.put((future, sub))
+
+        result = await asyncio.gather(*futures)
+        return result
+
+    async def _sender_worker(self):
+        while self._running:
+            batch = self._get_next_batch()
+            if not batch:
+                await asyncio.sleep(1.0)
+                continue
+            submissions = [sub for _, sub in batch]
+            try:
+                result = await self._judge(submissions)
+                for i, r in enumerate(result):
+                    batch[i][0].set_result(r)
+            except Exception as e:
+                for i, _ in enumerate(batch):
+                    batch[i][0].set_exception(e)
+
+    def _get_next_batch(self):
+        batch_submissions = []
+        while len(batch_submissions) < self.max_batch_size:
+            try:
+                sub = self._submission_queue.get_nowait()
+                batch_submissions.append(sub)
+                self._submission_queue.task_done()
+            except asyncio.QueueEmpty:
+                break
+        return batch_submissions
+
+    async def _judge(self, submissions: list[Submission]) -> list[SubmissionResult]:
+        if not submissions:
+            return []
+
+        n_submissions = len(submissions)
+        sub_ids = list(range(len(submissions)))
+        results = {}
+
+        while submissions:
+            logger.debug(f'Judging {len(submissions)} submissions.')
+            results = await _judge_batch_async(submissions, self._http)
+
+            queue_timeouts = []
+            for sub_id, result in zip(sub_ids, results):
+                if result.reason == 'queue_timeout':
+                    # Retry the submission later
+                    queue_timeouts.append((sub_id, submissions[sub_id]))
+                else:
+                    results[sub_id] = result
+
+            logger.debug(f'Processed {len(results)} submissions, Got {len(queue_timeouts)} timeouts.')
+
+            submissions = [sub for _, sub in queue_timeouts]
+            sub_ids = [sub_id for sub_id, _ in queue_timeouts]
+
+        return [results[i] for i in range(n_submissions)]
